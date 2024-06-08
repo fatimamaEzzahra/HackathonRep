6a02a72e93f8fab9c3fbf708d5eacf6395cf163e
ASSIYAAKBIB <assiaakbib3573@gmail.com>
2024-06-08 13:01:35 +0100

    chang

--- hackathon-frontend/src/Pages/Home.jsx
+++ hackathon-frontend/src/Pages/Home.jsx
@@ -1,10 +1,9 @@
 
-import HeroSwiper from '../Components/HomeComp/HeroSection'
+// import HeroSwiper from '../Components/HomeComp/HeroSection'
 import  {  StickyNavbar } from '../Components/HomeComp/Navbar'
 
 const Home = () => {
   return (
-    <div>
      <div style={{marginBottom:'40px'}}>
      <StickyNavbar/>
     </div>

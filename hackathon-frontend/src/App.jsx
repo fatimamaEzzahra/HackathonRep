28f0633ef5cf90ab7dbb99eabb41623aa3b113d3
FatimaEzzahra <fatizahra.elkawkabi@gmail.com>
2024-06-08 12:16:49 +0100

    navbar

--- hackathon-frontend/src/App.jsx
+++ hackathon-frontend/src/App.jsx
@@ -6,9 +6,7 @@ function App() {
 
   return (
     <>
-     <div className="App">
-      <h1 className="text-3xl font-bold underline">Hello world!</h1>
-    </div>
+     <Home/>
      
     </>
   )
---
+++ hackathon-frontend/src/Pages/Home.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { NavbarWithMegaMenu } from '../Components/HomeComp/Navbar'
+
+const Home = () => {
+  return (
+    <div>
+     <NavbarWithMegaMenu/>
+    </div>
+  )
+}
+
+export default Home
